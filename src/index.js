9def0820dc4c76ccfd2facea537dee349971a91d
artantstar <artantstar@gmail.com>
2021-07-26 18:26:22 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+  const words = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen",
+    20: "twenty",
+    30: "thirty",
+    40: "forty",
+    50: "fifty",
+    60: "sixty",
+    70: "seventy",
+    80: "eighty",
+    90: "ninety",
+  };
+
+  if (number < 100) {
+    if (number < 20 || number % 10 == 0) {
+      return (words[number]);
+    } else {
+      return (words[Math.floor(number / 10) * 10] + " " + words[number.toString().slice(-1)]);
+    }
+  } else if (number % 100 !== 0) {
+    return (words[Math.floor(number / 100)] + " hundred " + toReadable(parseInt(number.toString().slice(-2))));
+  } else {
+    return (words[(number / 100)] + " hundred");
+  }
 }
